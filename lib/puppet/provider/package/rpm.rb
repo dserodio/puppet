71ef05e043528f544bba490b6906f3e173fd6791
Daniel Serodio <dserodio@igcorp.com.br>
2013-06-02 23:42:37 -0300

    Add support for RPM virtual packages
    Without this patch the YUM package provider is able to install
    RPM whose name represents a "virtual package", but the RPM provider
    always reports that the package is not installed so YUM reports an
    installation error.

--- lib/puppet/provider/package/rpm.rb
+++ lib/puppet/provider/package/rpm.rb
@@ -69,7 +69,7 @@ Puppet::Type.type(:package).provide :rpm, :source => :rpm, :parent => Puppet::Pr
     #NOTE: Prior to a fix for issue 1243, this method potentially returned a cached value
     #IF YOU CALL THIS METHOD, IT WILL CALL RPM
     #Use get(:property) to check if cached values are available
-    cmd = ["-q", @resource[:name], "#{self.class.nosignature}", "#{self.class.nodigest}", "--qf", self.class::NEVRA_FORMAT]
+    cmd = ["-q", "--whatprovides", @resource[:name], "#{self.class.nosignature}", "#{self.class.nodigest}", "--qf", self.class::NEVRA_FORMAT]
 
     begin
       output = rpm(*cmd)
@@ -90,7 +90,7 @@ Puppet::Type.type(:package).provide :rpm, :source => :rpm, :parent => Puppet::Pr
       @resource.fail "RPMs must specify a package source"
     end
 
-    cmd = [command(:rpm), "-q", "--qf", self.class::NEVRA_FORMAT, "-p", source]
+    cmd = [command(:rpm), "-q", "--whatprovides", "--qf", self.class::NEVRA_FORMAT, "-p", source]
     h = self.class.nevra_to_hash(execfail(cmd, Puppet::Error))
     h[:ensure]
   end
